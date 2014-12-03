7f2cf09bcb69546c2e03b33058da33d0779d6f41
Chris <chris@gamiac.org>
2014-12-03 18:43:02 +0100

    Updated grit batch file; Added stereo helper

--- samples/barebone/barebone/assets/text/text.h
+++ samples/barebone/barebone/assets/text/text.h
@@ -1,12 +1,16 @@
 
 enum {
-     STRING_LANGUAGE,
-     STRING_PRECAUTION
+    STRING_LANGUAGE,
+    STRING_PRECAUTION
 };
 
 #include "lang_en.h"
 #include "lang_de.h"
 
+enum {
+    LANG_EN,
+    LANG_DE
+};
 char** languages[2] = {
 	lang_en, 
 	lang_de
--- system/batch/launch-grit.bat
+++ system/batch/launch-grit.bat
@@ -16,10 +16,30 @@ IF EXIST assets\images\ (
 
 	::CONVERT ALL IMAGES
 	FOR /r . %%F in (*.png *.bmp *.jpg *.jpeg *.gif *.pcx) do (
-		%VBDE%\tools\graphics\grit-0.8.6\grit.exe %%F -o _o\%%~nF.s  -ftc -gB2 -gu8 -m -mB16:p2hv_i11 -mLs -mu8 -mRtf -p!
+		%VBDE%\tools\graphics\grit-0.8.6\grit.exe %%F -o _o\%%~nF -ftc -gB2 -mB16:hv_i11 -mRtf -p!
 		ECHO #include "%%~nF.h" >> _o\_index.h
 	)
 )
 
 ::SWITCH TO ASSETS DIRECTORY
 PUSHD %PROJECT_DIR%
+
+::CONVERT ASSETS
+IF EXIST assets\spritesheets\ (
+
+	::SWITCH TO IMAGES DIRECTORY
+	PUSHD assets\spritesheets\
+
+	::PREPARATIONS
+	IF EXIST _o (
+		DEL _o\* /q
+	) ELSE (
+		MKDIR _o
+	)
+
+	::CONVERT ALL IMAGES
+	FOR /r . %%F in (*.png *.bmp *.jpg *.jpeg *.gif *.pcx) do (
+		%VBDE%\tools\graphics\grit-0.8.6\grit.exe %%F -o _o\%%~nF -ftc -gB2 -mB16:i11 -mR! -p!
+		ECHO #include "%%~nF.h" >> _o\_index.h
+	)
+)
