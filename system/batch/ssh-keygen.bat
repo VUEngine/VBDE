330804564f1a178c1e510ae6c346ba3d497d9977
Chris <chris@vr32.de>
2015-11-25 10:12:36 +0100

    Removed some old batch files

--- system/batch/ssh-keygen.bat
+++
@@ -1,1 +0,0 @@
-%VBDE%\cygwin\bin\bash -l -c "[ -f ~/.ssh/id_rsa ] || (echo 'SINCE THIS IS THE FIRST TIME YOU TRY TO ACCESS GIT FROM THIS ACCOUNT, YOU NEED TO CREATE A SSH KEY PAIR.' && ssh-keygen)"
