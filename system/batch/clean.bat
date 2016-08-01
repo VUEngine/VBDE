12ccfc16610ebacc0d572977ddd8b68d67ca3676
Chris <chris@vr32.de>
2016-08-01 21:41:53 +0200

    Added clean script

---
+++ system/batch/clean.bat
@@ -0,0 +1,13 @@
+::SET ECHO OFF
+@echo off
+
+ECHO Cleaning up project...
+
+::SCRIPT INITIALIZATION
+CALL %~dp0\init.bat
+
+::FIND PROJECT DIRECTORY
+CALL %VBDE%\system\batch\find-project-dir.bat %1
+
+::DELETE PROJECT'S BUILD DIRECTORY
+RMDIR /S /Q "%PROJECT_DIR%\build"
