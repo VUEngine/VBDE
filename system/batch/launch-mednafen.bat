31b94334bfc6ad8631e606ff226ae2aa7263c5b9
Christian Radke <chris@vr32.de>
2018-03-16 21:41:22 +0100

    Always bring the emulator window to the front after launching it

--- system/batch/launch-mednafen.bat
+++ system/batch/launch-mednafen.bat
@@ -1,1 +1,5 @@
+:: launch mednafen
 START "" "%VBDE%\tools\emus\mednafen-0.9.48-win32\mednafen.exe" "%PROJECT_DIR%\build\output.vb"
+
+:: bring emulator window to front and center it
+%VBDE%\system\tools\nircmd\nircmd.exe win activate center title "output"
